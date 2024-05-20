6bfee70876598c50423c2b97ea2ca9d1caa1b1ab
Clone888 <dwclone@gmail.com>
2024-05-20 09:27:36 +0200

    Initial commit

--- ui-tests/specs/step_definitions/correct-price.js
+++ ui-tests/specs/step_definitions/correct-price.js
@@ -4,4 +4,3 @@ import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 Then('I should see the price {string} for the product {string}', (price, productName) => {
   cy.get('.product').contains('div.product', productName).find('.price').contains('Pris: ' + price + ' kr')
 });
-
